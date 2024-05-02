99566c17782b4c6a38753d41a0a89768166d157e
MariamHany4 <mar.hany@hotmail.com>
2024-05-02 17:57:38 +0300

    Updating GPS_module.c

--- Source/HAL/GPS_module.c
+++ Source/HAL/GPS_module.c
@@ -123,5 +123,36 @@ else if(strcmp(logElements[5],"W")==0)
 
 
 }
+				float toDegree(float angle)
+					{
+						float angleindegree=(int)angle/100;
+						float mins = angle -(float)angleindegree*100;
+						return (angleindegree + (mins/60));
+					}
 
+			float toRad(float angle)
+			{
+				return ((angle * PI) / 180);
+			}
+
+			float harversine(float LongAinRad, float LatAinRad, float LongBinRad, float LatBinRad)
+			{
+					//we will use Haversine law to calculate the distance on sphere
+					float x=pow(sin((LatBinRad-LatAinRad)/2),2);
+					float y=pow(sin((LongBinRad-LongAinRad)/2),2);
+					float a=x+y*cos(LatAinRad)*cos(LatBinRad);
+					float c = 2*atan2(sqrt(a),sqrt(1-a));
+					return earthradius*c;
+			}
+			float obtainDistance(float LongA, float LatA, float LongB, float LatB)
+				{
+				//first we need to convert the distance given to radian
+					float LatAinRad=toRad(toDegree(LatA));
+					float LatBinRad=toRad(toDegree(LatB));
+					float LongAinRad=toRad(toDegree(LongA));
+					float LongBinRad=toRad(toDegree(LongB));
+					//now we have them in radian
+					//now we call Harversine method to get the distance
+					return harversine(LongAinRad,LatAinRad,LongBinRad,LatBinRad);
+			}
 
